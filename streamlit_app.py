0364954a9bb952aeac2c74fff6abe1b387a88b33
Adrien Treuille <adrien.g.treuille@gmail.com>
2020-01-27 13:46:00 -0800

    Got loading of repo tags from github API server working.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,32 @@
 import streamlit as st
+import requests
 
-"""# Streamlit Release Notes App"""
+_cached_get = st.cache(requests.get)
 
+def get_json(url, **kwargs):
+    """Cached function to get json from Github."""
+    response = _cached_get(url, **kwargs)
+    response.raise_for_status()
+    return response.json()
+
+def get_from_github(get):
+    assert not get.startswith('/')
+    url = 'https://api.github.com/' + get
+    headers = {
+        'Accept': 'application/vnd.github.inertia-preview+json',
+    }
+    return get_json(url, headers=headers)
+
+def main():
+    """Main String."""
+
+    # Dipslay the header
+    """# Streamlit Release Notes App"""
+    # testing some queries
+    queries = ['users/treuille', 'orgs/streamlit/repos', 'repos/streamlit/streamlit/tags']
+    for query in queries:
+        st.write(f'`{query}`')
+        st.json(get_from_github(query))
+
+if __name__ == '__main__':
+    main()
--- todo.md
+++ todo.md
@@ -1,4 +1,12 @@
-# Todo
+# Streamlit Release Note Apps
+
+## Todo
 
 - see if I can parse the versions
-- 
+- see if I can cache requests to github
+- see if I can list the pull requests that went into a version
+
+## Docs
+
+- <https://developer.github.com/v3/>
+- <https://requests.readthedocs.io/en/master/user/quickstart/>
