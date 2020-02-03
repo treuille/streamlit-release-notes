937242c9529f6a2acd5455bcb10c22171455eb9d
Adrien Treuille <adrien.g.treuille@gmail.com>
2020-02-03 09:42:27 -0800

    Added some text.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,13 @@ import streamlit as st
 """
 # Demo for Insight
 
-This project has url:
+_This project_ has url:
 
 <https://github.com/treuille/streamlit-release-notes>
+
+Therefore, _your Insight URL_ will be
+
+<https://insight2020a.streamlit.io/treuille/streamlit-release-notes/>
 """
 
 if st.button('Show ballons'):
